1a57ed51ed579a0fd2433a46d6631fbb28cadc72
Akashmahto022 <akashmahto1169@gmail.com>
2024-12-28 20:14:15 +0530

    learn about data type in cpp

--- variables/variable.cpp
+++ variables/variable.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main(){
     int age = 21;
-    char name = "Akash Mahto"
-    cout << age + name;
+    char grade = 'A';
+    float PI = 3.14f;
+
+    bool isTrue = false;
+    double price = 120.25;
+
+    cout << sizeof(age);
+    cout << grade;
     return 0;
 }
