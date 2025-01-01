db1fc7b666341aa87afb76d692af8fbc097a3978
Akashmahto022 <akashmahto1169@gmail.com>
2025-01-01 11:35:51 +0530

    learn about input in cpp

--- input/input.cpp
+++ input/input.cpp
@@ -7,6 +7,13 @@ int main(){
     cin >> a;
     cout << "enter your no b : ";
     cin >> b;
+    cout << "Enter your Name : ";
+    string c;
+
+    cin >> c;
 
     cout << "your total is : " << (a + b) << endl;
+    cout << "your name is: " << c << endl;
 }
+
+
